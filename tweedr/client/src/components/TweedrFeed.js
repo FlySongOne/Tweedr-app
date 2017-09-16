e81e8743e459751eeb6f357f628b416a0e0f89d6
Yan Zhuk <yan.zhuk45@gmail.com>
2017-09-16 17:09:59 -0400

    add TweedFeed, Tweed, TweedForm

--- tweedr/client/src/components/TweedrFeed.js
+++ tweedr/client/src/components/TweedrFeed.js
@@ -1,1 +1,19 @@
+import React, { Component } from 'react';
+import Tweed from './Tweed';
 
+class TweedrFeed extends Component {
+
+
+  render() {
+    console.log('TweedrFeed rendering');
+    return (
+      <div id="TweedrFeedDiv">
+        { this.props.data.map( tweed => {
+          return <Tweed tweed={tweed} key={tweed.id} />
+        })}
+      </div>
+    )
+  }
+}
+
+export default TweedrFeed;
