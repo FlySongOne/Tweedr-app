a803f290fdcd88d9de6b947c4b8c8aef9aeab9ec
Kyung Song <kyunghan.song@gmail.com>
2017-09-16 14:40:43 -0400

    handling submit

--- tweedr/client/src/App.js
+++ tweedr/client/src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(){
+      super();
+      this.state = {
+         tweeds :[],
+         inputContentValue :'',
+      }
+
+ //     this.handleInputContentValue = this.handleInputContentValue.bind(this);
+      this.handleTweedsSubmit = this.handleTweedsSubmit.bind(this);
+  }
+
+  handleTweedsSubmit(){
+
+     axios('http://localhost:3001/api/tweeds/')
+       .then(res => {
+
+           this.setState({
+             tweeds: res.data.data.tweeds,
+            //  tweed_text: res.data.data.tweeds,
+           })
+           console.log(this.state.tweed_text);
+       })
+  }
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+         <input id='input'/>
+         <button onClick={this.handleTweedsSubmit}>Press this</button>
+
       </div>
     );
   }
