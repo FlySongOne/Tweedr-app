f2afdc66fbb78be70ea6dce69aa1c08fba0e90c8
Kyung Song <kyunghan.song@gmail.com>
2017-09-16 16:24:23 -0400

    app.js

--- tweedr/client/src/App.js
+++ tweedr/client/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import TweedrForm from './components/TweedrForm';
+import TweedrFeed from './components/TweedrFeed';
 
 class App extends Component {
   constructor(){
@@ -11,30 +13,63 @@ class App extends Component {
          inputContentValue :'',
       }
 
- //     this.handleInputContentValue = this.handleInputContentValue.bind(this);
+      this.handleInputContentValue = this.handleInputContentValue.bind(this);
       this.handleTweedsSubmit = this.handleTweedsSubmit.bind(this);
+  }
+
+  componentDidMount(){
+     axios('http://localhost:3001/api/tweeds/')
+        .then(res=> {
+            this.setState(prevState=>{
+
+               return{
+                  tweeds:res.data.data.tweeds,
+               }
+            });
+
+        });
   }
 
   handleTweedsSubmit(){
 
-     axios('http://localhost:3001/api/tweeds/')
-       .then(res => {
+     axios.post('http://localhost:3001/api/tweeds/', {
+      tweed: this.state.tweed.tweed_text,
+     })
+     .then(res => {
+      {
+         const tweed ={
+           tweeds: res.data.data.tweeds,
+         }
+        //  tweed_text: res.data.data.tweeds,
 
-           this.setState({
-             tweeds: res.data.data.tweeds,
-            //  tweed_text: res.data.data.tweeds,
-           })
-           console.log(this.state.tweed_text);
-       })
+        //   console.log(this.state.tweeds[0].tweed_text);
+
+        this.setState((prevState)=>{
+            return{
+                tweeds: prevState.tweeds.concat(tweed)
+            }
+        })
+     }
+    }).catch(err =>console.log(err));
+ }
+
+  handleInputContentValue(event) {
+    this.setState({
+      inputContentValue: event.target.value
+    });
   }
-
 
   render() {
     return (
       <div className="App">
-         <input id='input'/>
-         <button onClick={this.handleTweedsSubmit}>Press this</button>
-
+         <TweedrForm
+             handleTweedsSubmit={this.handleTweedsSubmit}
+             handleInputContentValue={this.handleInputContentValue}
+             inputContentValue={this.state.inputContentValue}
+         />
+         <TweedrFeed
+             data={this.state.tweeds}
+         />
       </div>
     );
   }
